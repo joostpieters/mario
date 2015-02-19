20b1f140f2bc64af952232d0f91660bfa54daf58
pieter.vdberghe@outlook.com <pieter.vdberghe@outlook.com>
2015-02-19 16:34:44 +0100

    heel het bestand van toledo erop gezet en het skelet van mazub al uitgeschreven

---
+++ OGP1415-JumpingAlien/src/Mazub.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ * @author Pieter
+ *
+ */
+public class Mazub {
+	
+	/**
+	 * NOMINAAL uitwerken
+	 */
+	public void startMove() {
+	
+	}
+	
+	/**
+	 * NOMINAAL uitwerken
+	 */
+	public void endMove() {
+		
+	}
+	
+	/**
+	 * DEFENSIEF uitwerken
+	 */
+	public void advanceTime() {
+		
+	}
+	
+	/**
+	 * TOTAAL uitwerken
+	 */
+	public void startJump() {
+		
+	}
+	
+	/**
+	 * TOTAAL uitwerken
+	 */
+	public void endJump() {
+		
+	}
+	
+	/**
+	 * DEFENSIEF uitwerken
+	 */
+	public void startDuck() {
+		
+	}
+	
+	/**
+	 * DEFENSIEF uitwerken
+	 */
+	public void endDuck() {
+		
+	}
+	
+	/**
+	 * NOMINAAL uitwerken
+	 * GEEN formele documentatie nodig
+	 */
+	public void getCurrentSprite() {
+		
+	}
+}
