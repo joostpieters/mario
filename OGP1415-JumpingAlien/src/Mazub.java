3d0f2a649ceb882317263fd43751d40c2f2b4767
pieter.vdberghe@outlook.com <pieter.vdberghe@outlook.com>
2015-02-19 16:59:07 +0100

    heel het project op git

--- OGP1415-JumpingAlien/src/Mazub.java
+++ OGP1415-JumpingAlien/src/Mazub.java
@@ -1,64 +1,6 @@
-/**
- * 
- * @author Pieter
- *
- */
+
 public class Mazub {
-	
-	/**
-	 * NOMINAAL uitwerken
-	 */
-	public void startMove() {
-	
-	}
-	
-	/**
-	 * NOMINAAL uitwerken
-	 */
-	public void endMove() {
-		
-	}
-	
-	/**
-	 * DEFENSIEF uitwerken
-	 */
-	public void advanceTime() {
-		
-	}
-	
-	/**
-	 * TOTAAL uitwerken
-	 */
-	public void startJump() {
-		
-	}
-	
-	/**
-	 * TOTAAL uitwerken
-	 */
-	public void endJump() {
-		
-	}
-	
-	/**
-	 * DEFENSIEF uitwerken
-	 */
-	public void startDuck() {
-		
-	}
-	
-	/**
-	 * DEFENSIEF uitwerken
-	 */
-	public void endDuck() {
-		
-	}
-	
-	/**
-	 * NOMINAAL uitwerken
-	 * GEEN formele documentatie nodig
-	 */
-	public void getCurrentSprite() {
+	public void test22() {
 		
 	}
 }
