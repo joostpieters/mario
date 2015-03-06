37e84d314dfbbdf4580d274970948cc5fdf95db1
Eduw <ward_romanus@hotmail.com>
2015-03-06 18:57:53 +0100

    Revert "Fuck miserie"
    This reverts commit 690afdb77d79dd72920bf977eab2149f29fe0724.

--- OGP1415-JumpingAlien/src-provided/jumpingalien/part1/facade/Facade.java
+++ OGP1415-JumpingAlien/src-provided/jumpingalien/part1/facade/Facade.java
@@ -1,9 +1,5 @@
 package jumpingalien.part1.facade;
 
-import jumpingalien.model.Mazub;
-
 public class Facade implements IFacade {
-	public double[] getAcceleration(Mazub alien){
-		return new double[] {alien.getAcc(), alien.getYAcc()};
-	}
+
 }
