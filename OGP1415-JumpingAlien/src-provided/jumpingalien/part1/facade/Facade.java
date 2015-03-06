690afdb77d79dd72920bf977eab2149f29fe0724
Eduw <ward_romanus@hotmail.com>
2015-03-06 18:57:32 +0100

    Fuck miserie

--- OGP1415-JumpingAlien/src-provided/jumpingalien/part1/facade/Facade.java
+++ OGP1415-JumpingAlien/src-provided/jumpingalien/part1/facade/Facade.java
@@ -1,5 +1,9 @@
 package jumpingalien.part1.facade;
 
-public class Facade implements IFacade {
+import jumpingalien.model.Mazub;
 
+public class Facade implements IFacade {
+	public double[] getAcceleration(Mazub alien){
+		return new double[] {alien.getAcc(), alien.getYAcc()};
+	}
 }
