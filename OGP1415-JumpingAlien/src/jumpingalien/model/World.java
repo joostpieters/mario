94550a449d620ccf45da4f96d0e1d7ee3a0a98f2
pieter.vdberghe@outlook.com <pieter.vdberghe@outlook.com>
2015-03-18 18:05:01 +0100

    class world aangemaakt

---
+++ OGP1415-JumpingAlien/src/jumpingalien/model/World.java
@@ -0,0 +1,37 @@
+package jumpingalien.model;
+
+public class World {
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private int getX() {
+		return X;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private int getY() {
+		return Y;
+	}
+	
+	/**
+	 * 
+	 * @param position
+	 * @return
+	 */
+	private Mazub pixelPositioned(int[] position) {
+		
+	}
+	
+	/**
+	 * NO DOCUMENTATION MUST BE WORKED OUT
+	 */
+	private void advanceTime() {
+		
+	}
+
+}
