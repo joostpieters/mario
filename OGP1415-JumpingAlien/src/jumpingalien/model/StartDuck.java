9808df82fc14e3a3775a010a42632980d013d59d
pieter.vdberghe@outlook.com <pieter.vdberghe@outlook.com>
2015-05-09 22:15:13 +0200

    wat actionstatement maar die statments snap ik toch nog niet helemaal

---
+++ OGP1415-JumpingAlien/src/jumpingalien/model/StartDuck.java
@@ -0,0 +1,18 @@
+package jumpingalien.model;
+
+import java.util.Map;
+
+public class StartDuck extends ActionStatement {
+
+	public StartDuck(Statement action, Mazub alien) {
+		super(action, alien);
+	}
+
+	@Override
+	public Map<String, Type> execute(Map<String, Type> var) {
+		
+		return null;
+	}
+	
+
+}
