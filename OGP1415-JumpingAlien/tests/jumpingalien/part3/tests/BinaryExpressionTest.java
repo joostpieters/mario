c98152151e978c9342fd04d7338f04c0bca6eb15
pieter.vdberghe@outlook.com <pieter.vdberghe@outlook.com>
2015-05-16 16:57:50 +0200

    geen zin meer om te zoeken

---
+++ OGP1415-JumpingAlien/tests/jumpingalien/part3/tests/BinaryExpressionTest.java
@@ -0,0 +1,10 @@
+package jumpingalien.part3.tests;
+import org.junit.Test;
+
+public class BinaryExpressionTest {
+	
+	@Test
+	public void testAddition() {
+		
+	}
+}
---
+++ OGP1415-JumpingAlien/tests/jumpingalien/part3/tests/BuzamTest.java
@@ -0,0 +1,9 @@
+package jumpingalien.part3.tests;
+import org.junit.Test;
+
+public class BuzamTest {
+	
+	
+	
+
+}
