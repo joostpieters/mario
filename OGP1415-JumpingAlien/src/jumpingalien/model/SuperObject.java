00d7692145d32c3632a32d41c6dd855fbbcc4898
pieter.vdberghe@outlook.com <pieter.vdberghe@outlook.com>
2015-05-19 15:34:10 +0200

    push anders is nekeer

--- OGP1415-JumpingAlien/src/jumpingalien/model/SuperObject.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/SuperObject.java
@@ -1,5 +1,8 @@
 package jumpingalien.model;
 
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Raw;
+
 public abstract class SuperObject {
 	
 	public SuperObject() {
@@ -12,6 +15,33 @@ public abstract class SuperObject {
 	public abstract int getXDim();
 	public abstract int getYDim();
 	public abstract int getHitpoints();
+	
+	/**
+	 * the world where the superobject appears
+	 */
+	private World world;
+	/**
+	 * returns the world where the superobject appears
+	 * @return world
+	 */
+	@Basic @Raw
+	public World getWorld() {
+		return this.world;
+	}
+	/**
+	 * Sets the world to the givan value world
+	 * @param world
+	 * 		the world to set
+	 * @pre the given World world may not be null
+	 * 			| world != null
+	 * @post world is set
+	 * 			| this.world = world
+	 */
+	@Raw
+	public void setWorld(World world) {
+		assert world != null;
+		this.world = world;
+	}	
 
 
 }
